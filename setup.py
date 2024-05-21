3c043127895a36e83780f9b9bfe3660bf20a7556
GaelicGrime <will.angus.blaylock@gmail.com>
2024-05-21 05:58:54 +0000

    20240520_235854

--- setup.py
+++ setup.py
@@ -14,13 +14,18 @@ from setuptools import setup
 from datetime import datetime as DT
 
 
+__VERSION__ = DT.now().strftime("%Y.%m.%d.%H%M%S")
+with open("cliTimer/__VERSION__.py", "tw") as _FD_OUT_:
+  _FD_OUT_.write(f"""\n\n__VERSION__ = \"{__VERSION__}\"\n\n#\n""")
+
+
 setup(
   author="GaelicGrime",
   author_email="will.angus.blaylock@gmail.com",
   license="GPLv3",
   name="cliTimer",
   url="https://github.com/ComfortableSoftware/cliTimer",
-  version=VS.VERSION,
+  version=__VERSION__,
   keywords=[
       "CLI",
       "Command line",
@@ -35,6 +40,8 @@ setup(
   packages=find_packages(),
   install_requires=[
       "CSCF",
+      "datedelta",
+      "python-dateutil",
   ],
   extras_require={
   },
